32bef1db1c0ab45e52f5647c8affcaf0e212994a
Joël Poulin <joel@oepa.ch>
2020-09-14 19:02:22 +0200

    kept only the composition-api plugin mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,85 @@
-import plugin from './plugin'
-import { useI36n, provideI36n } from './useI36n'
-import { provideI18n, useI18n } from './example'
+import { ref, reactive, watch, computed, provide, inject } from '@vue/composition-api'
+import md from 'snarkdown'
+import { replace, extractPlaceholders } from '@jota-one/replacer'
 
-export { useI36n, provideI36n, plugin, provideI18n, useI18n }
-export default plugin
+const i36nSymbol = Symbol()
+
+const provideI36n = function (language, { load }) {
+  const loaded = ref(0)
+  const labels = reactive({})
+  const currentLanguage = ref(language)
+  const showKey = ref(false)
+
+  const _key = (language, key) => {
+    if (!labels[language]) {
+      return `{${key}}`
+    }
+    if (Array.isArray(labels[language][key])) {
+      return labels[language][key].map((item, index) => {
+        const placeholders = extractPlaceholders(item)
+        let plh = ''
+        if (placeholders.length > 0) {
+          plh = ', [' + placeholders.join(',') + ']'
+        }
+        return `{${key}[${index}]${plh}}`
+      })
+    }
+    const placeholders = extractPlaceholders(labels[language][key])
+    let plh = ''
+    if (placeholders.length > 0) {
+      plh = ', [' + placeholders.join(',') + ']'
+    }
+    return `{${key}${plh}}`
+  }
+
+  const _format = (value, params, source) => {
+    if (Array.isArray(value)) {
+      return value.map(v => md(replace(v, params, source)))
+    }
+    return md(replace(value, params, source))
+  }
+
+  const $label = computed(() => {
+    if (loaded.value === 0) {
+      return () => ' '
+    }
+
+    const langLabels = labels[currentLanguage.value]
+
+    if (showKey.value) {
+      return key => _key(currentLanguage.value, key)
+    }
+
+    return langLabels
+      ? (key, params) => _format(langLabels[key], params, langLabels) || `{${key}}`
+      : () => ' '
+  })
+
+  let _switching = false
+  watch(() => currentLanguage.value, async ln => {
+    if (ln && !_switching) {
+      _switching = true
+
+      if (!labels[ln]) {
+        labels[ln] = await load(ln)
+        loaded.value += 1
+      }
+
+      _switching = false
+    }
+  }, { immediate: true })
+
+  const i36n = {
+    language: currentLanguage,
+    showKey,
+    $label
+  }
+
+  provide(i36nSymbol, i36n)
+}
+
+const useI36n = function () {
+  return inject(i36nSymbol)
+}
+
+export { useI36n, provideI36n }
