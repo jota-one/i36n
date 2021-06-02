3a12e8a22d5811ac2d909db7062591600f9e69f5
Joël Poulin <joel@oepa.ch>
2021-06-02 17:59:18 +0200

    made it compatible with vue2 and vue3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ref, reactive, watch, computed, provide, inject } from '@vue/composition-api'
+import { ref, reactive, watch, computed, provide, inject } from 'vue-demi'
 import md from 'snarkdown'
 import { replace, extractPlaceholders } from '@jota-one/replacer'
 
